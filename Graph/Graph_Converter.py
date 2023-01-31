4beade40c1dcb0680a50b1753c751016711add94
Andrew Adel <andrewadel3322@gmail.com>
2023-01-31 10:43:37 +0200

    shift read_XML method to be static method in GraphOfUsers class

--- Graph/Graph_Converter.py
+++ Graph/Graph_Converter.py
@@ -1,11 +1,11 @@
-from read_xml import read_XML, graphUser, userPost
+from read_xml import graphUser, userPost
 NULL_EDGE = 0
 
 
 class GraphOfUsers:
     def __init__(self,max:int, path:str) -> None:
         #lsit of users token from xml
-        users:list[graphUser] = read_XML(path)
+        users:list[graphUser] = self.read_XML(path)
         #max number of users can graph contain
         self.max = max
         #number of existing users now in graph
@@ -146,5 +146,85 @@ class GraphOfUsers:
                     pass
                 pass
         pass
+
+
+    @staticmethod
+    def read_XML(path: str):
+        file = open(path, 'r')
+
+        comment = False
+        userName = ""
+        userID:int
+        userFollowed = []
+        userPosts = []
+        postBody = ""
+        postTopics = []
+        tagValue:str = ""
+        tagStack = []
+        startValue:int
+        endValue:int
+        lines = ""
+        users = []
+        for line in file:
+            lines += line.strip()
+            pass
+
+        for ind in range(0,len(lines)):
+            if lines[ind] == '<' and not comment:
+                #check if there is unclosed tag before
+                if len(lines) > (ind + 3) and  lines[ind+1:ind+4] == '!--': 
+                    comment = True
+                    continue
+                #clear tagName
+                tagName = ""
+                pass
+
+
+            elif lines[ind] == '>':
+                #check end of comment
+                if ind > 3 and lines[ind-2:ind] == '--': 
+                    comment = False
+                    continue
+                if comment: continue
+
+                #end of openning tag
+                if tagName[0] != '/':
+                    startValue = ind+1
+                    tagStack.append(tagName)
+
+                #end of closing tag
+                else:
+                    #get the begin and end of value to use it in body, name, id, and topic tags  
+                    endValue = ind - len(tagName)-1
+                    tagValue = lines[startValue:endValue]
+                    if tagStack[-1] == tagName[1:]: tagStack.pop()
+
+                    if tagName == "/id":
+                        if tagStack[-1] == "user":
+                            userID = int(tagValue)
+                        elif tagStack[-1] == "follower":
+                            userFollowed.append(int(tagValue))
+                    elif tagName == "/name":
+                        userName = tagValue
+                    elif tagName == "/body":
+                        postBody = tagValue
+                    elif tagName == "/topic":
+                        postTopics.append(tagValue)
+                    elif tagName == "/post":
+                        userPosts.append(userPost(postBody,postTopics))
+                        postTopics = []
+                        postBody = ""
+                    elif tagName == "/user":
+                        users.append(graphUser(userID,userName,userPosts,userFollowed))
+                        userPosts = []
+                        userFollowed = []
+                tagName = ""                
+                
+            # take the name of tag 
+            else:
+                if not comment:
+                    tagName = tagName + lines[ind] 
+            pass
+        return users
 
     pass
--- Graph/test_Graph_Converter.py
+++ Graph/test_Graph_Converter.py
@@ -1,7 +1,7 @@
 from Graph_Converter import GraphOfUsers
-path = "sample.xml"
+path = "Graph\sample.xml"
 graph = GraphOfUsers(5,path)
 followed = graph.getUserFollowedList(graph.getUserFromId(1))
 followers = graph.getUserFollowerList(graph.getUserFromId(1))
-graph.BFT(graph.getUserFromId(2))
+graph.BFT(graph.getUserFromId(5))
 print("Finish !")
